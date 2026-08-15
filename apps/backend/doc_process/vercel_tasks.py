e06d0f8464f88ebe92f78efff6ab13c2eaef356d
alirezabg18 <abgo44@gmail.com>
2026-08-15 23:36:52 +0330

    Fix Celery task registration

--- apps/backend/doc_process/vercel_tasks.py
+++ apps/backend/doc_process/vercel_tasks.py
@@ -1,31 +1,33 @@
 from config.celery import app
 
-from . import tasks as core_tasks
-
 
 @app.task(
     name="doc_process.tasks.run_document_parsing_task"
 )
 def run_document_parsing_task(document_id: int):
-    return core_tasks.run_document_parsing_task(document_id)
+    from doc_process.tasks import run_document_parsing_task as task_impl
+    return task_impl(document_id)
 
 
 @app.task(
     name="doc_process.tasks.run_document_review_job_task"
 )
 def run_document_review_job_task(review_job_id: int):
-    return core_tasks.run_document_review_job_task(review_job_id)
+    from doc_process.tasks import run_document_review_job_task as task_impl
+    return task_impl(review_job_id)
 
 
 @app.task(
     name="doc_process.tasks.run_block_difference_task"
 )
-def run_block_difference_task(review_job_id: int):
-    return core_tasks.run_block_difference_task(review_job_id)
+def run_block_difference_task(*args, **kwargs):
+    from doc_process.tasks import run_block_difference_task as task_impl
+    return task_impl(*args, **kwargs)
 
 
 @app.task(
     name="doc_process.tasks.run_ai_review_task"
 )
-def run_ai_review_task(document_id: int):
-    return core_tasks.run_ai_review_task(document_id)
+def run_ai_review_task(*args, **kwargs):
+    from doc_process.tasks import run_ai_review_task as task_impl
+    return task_impl(*args, **kwargs)
